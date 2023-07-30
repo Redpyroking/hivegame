3d934be8360e9d3d318fb79a91c5c49a2565d800
Redpyroking <49547325+Redpyroking@users.noreply.github.com>
2023-07-30 23:05:52 +0530

    add main python file

---
+++ game.py
@@ -0,0 +1,27 @@
+import pygame
+
+pygame.init()
+
+pygame.display.set_caption("Game")
+
+BG_COLOR = (255,255,255)
+WIDTH = 640
+HEIGHT = 320
+FPS = 60
+
+window = pygame.display.set_mode((WIDTH,HEIGHT))
+
+def main(window):
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+    pygame.quit()
+    quit()
+
+if __name__ == "__main__":
+    main(window)
